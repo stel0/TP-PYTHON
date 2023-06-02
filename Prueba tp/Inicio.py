9bc7758b68ab585a22ff3f9ee1caa25fdc0ffc95
atighu <112597615+atighu@users.noreply.github.com>
2023-06-02 14:48:59 -0400

    ultimo pull atilio

--- Prueba tp/Inicio.py
+++ Prueba tp/Inicio.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt5.uic import loadUi
 from main_window import MainWindow as mw
+from PyQt5.QtCore import QUrl
+from PyQt5.QtGui import QDesktopServices
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
@@ -14,12 +16,18 @@ class MainWindow(QMainWindow):
         self.Informes.clicked.connect(self.open_file_dialog)
         self.Salir.clicked.connect(self.salir)
         self.Ver_main_window.clicked.connect(self.ver)
+        self.Ayuda.clicked.connect(self.abrir_pdf)
     def open_file_dialog(self):
         # Abrir el diálogo de selección de archivos
         file_dialog = QFileDialog()
         file_dialog.setFileMode(QFileDialog.AnyFile)
         file_dialog.exec_()
+    def abrir_pdf(self):
+        # Ruta al archivo PDF
+        pdf_path = "Primer_final.pdf"
 
+        # Abrir el archivo PDF con la aplicación predeterminada del sistema
+        QDesktopServices.openUrl(QUrl.fromLocalFile(pdf_path))
     def salir(self):
         QApplication.quit()
 
--- Prueba tp/main_window.py
+++ Prueba tp/main_window.py
@@ -1,10 +1,11 @@
-from PyQt5.QtWidgets import QApplication,QFileDialog, QMainWindow, QWidget
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsTextItem, QGraphicsRectItem, QApplication, QFileDialog, QMainWindow, QWidget
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import pyqtSignal
 from Database import Database
 from Organigrama import Organigrama
 from Dependencia import Dependencia
 from Persona import Persona
+from PyQt5.QtCore import Qt, QPointF
 
 DATABASE = "base.db"
 database = Database(DATABASE)
@@ -14,7 +15,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setWindowTitle("Mi Aplicación")
-
+        self.qgv_scene = QGraphicsScene()
         # Cargar el archivo .ui
         loadUi("main_window_1.ui", self)
         database.connect()
@@ -28,13 +29,18 @@ class MainWindow(QMainWindow):
         self.crear_organigrama.clicked.connect(self.create_organigrama)
         self.abrir_organigrama.clicked.connect(self.open_organigrama)
         self.Add_Persona.clicked.connect(self.abrir_form_persona)
+        
     def buttonClicked(self):
         # ...
         self.form_window = FormDependencia()  # Create an instance of FormDependencia
+        # self.form_window.enviar_dependencia_signal.connect(self.add_dependencia_rect)
         self.form_window.show()  # Show the form window
+        
     def create_organigrama(self):
         self.form_organigrama = FormOrganigrama()
+        self.form_organigrama.enviar_organigrama_signal.connect(self.add_rect_slot)
         self.form_organigrama.show()
+        
     def open_organigrama(self):
         file_dialog = QFileDialog()
         filename, _ = file_dialog.getOpenFileName(self, "Abrir Organigrama", "", "Archivos de Imagen (*.png *.jpg *.jpeg)")
@@ -44,8 +50,43 @@ class MainWindow(QMainWindow):
     def abrir_form_persona(self):
         self.form_persona = FormPersona()
         self.form_persona.show()
+        
+
+    def add_rect_slot(self, titulo, fecha):
+        rect = QGraphicsRectItem()
+        rect.setRect(0, 0, 200, 100)
+        rect.setPos(50, 50)
+        rect.setFlag(QGraphicsRectItem.ItemIsMovable)  # Hacer que el rectángulo sea movible
+
+        rect.setBrush(Qt.white)  # Establecer el fondo del rectángulo como blanco
+
+        text = QGraphicsTextItem(rect)  # Hacer que el rectángulo sea el padre del texto
+        text.setDefaultTextColor(Qt.black)  # Establecer el color del texto como negro
+        text.setPlainText(f"Título: {titulo}\nFecha: {fecha}")
+        text.setPos(rect.rect().topLeft() + QPointF(10, 10))  # Posicionar el texto dentro del rectángulo
+
+        self.qgv_scene.addItem(rect)
+        self.qgv_scene.addItem(text)
+        self.qgv.setScene(self.qgv_scene)
+    # def add_dependencia_rect(self, titulo, apellido, nombre):
+    #     rect = QGraphicsRectItem()
+    #     rect.setRect(0, 0, 200, 100)
+    #     rect.setPos(50, 50)
+    #     rect.setFlag(QGraphicsRectItem.ItemIsMovable)
+
+    #     rect.setBrush(Qt.white)
+
+    #     text = QGraphicsTextItem(rect)
+    #     text.setDefaultTextColor(Qt.black)
+    #     text.setPlainText(f"Título: {titulo}\nApellid{apellido}\nNombre:{nombre}")
+    #     text.setPos(rect.rect().topLeft() + QPointF(10, 10))
+
+    #     self.qgv_scene.addItem(rect)
+    #     self.qgv_scene.addItem(text)
+    #     self.qgv.setScene(self.qgv_scene)
+
 class FormOrganigrama(QWidget):
-    enviar_organigrama = pyqtSignal(str)
+    enviar_organigrama_signal = pyqtSignal(str, str)
 
     def __init__(self, parent=None):
         super(FormOrganigrama, self).__init__(parent)
@@ -62,10 +103,12 @@ class FormOrganigrama(QWidget):
         org = Organigrama(titulo, fecha)
         database.connect()
         database.insertarData("Organigrama", org.get_dict())
+        self.enviar_organigrama_signal.emit(titulo, fecha)
         self.close()
         database.disconnect()
 
 class FormDependencia(QWidget):
+    # enviar_dependencia_signal = pyqtSignal(str, str,str)
     def __init__(self):
         super(FormDependencia, self).__init__()
         self.setWindowTitle("Formulario dependencia")
@@ -100,6 +143,8 @@ class FormPersona(QWidget):
 
     def e_persona(self):
         self.close()
+
+
 if __name__ == '__main__':
     app = QApplication([])
     window = MainWindow()
